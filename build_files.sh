6f74593fb876a1114a50ce2430d8cc1cfb0de1e0
kristianvv <kristianvaag@gmail.com>
2025-03-24 11:28:52 +0100

    build

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,14 @@
 echo "BUILD START"
 
+# Install pipenv if not available
+pip install --upgrade pipenv
+
+# Install dependencies from Pipfile.lock
+pipenv install --deploy
+
+# Run Django collectstatic
+pipenv run python manage.py collectstatic --noinput
+
 python3.10 -m pip install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
 
