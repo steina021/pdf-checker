4ee5abaa37c7b8ad1ddf6e460473cb62117768f7
kristianvv <kristianvaag@gmail.com>
2025-03-24 10:57:49 +0100

    build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
