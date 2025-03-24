88f4872cb69d50962a1c8a53544dec9d6ff46459
kristianvv <kristianvaag@gmail.com>
2025-03-24 11:49:14 +0100

    vercel test

--- build_files.sh
+++ build_files.sh
@@ -3,13 +3,10 @@ echo "BUILD START"
 # Install pipenv if not available
 pip install --upgrade pipenv
 
-# Install dependencies from Pipfile.lock
-pipenv install --deploy
+# Install dependencies from Pipfile.lock (using pipenv)
+pipenv install --deploy --ignore-pipfile
 
-# Run Django collectstatic
+# Activate the virtual environment and collect static files
 pipenv run python manage.py collectstatic --noinput
-
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
